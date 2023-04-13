d4bd6f5e5a9a382077ad6b751d80700eb8f69dfa
manoti <manotimike@gmail.com>
2023-04-13 18:02:44 +0300

    header component added

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <img
+        src='https://api.sofascore.app/api/v1/team/38/image'
+        alt=''
+        className='logo'
+      />
+      <h1>Chelsea Recent Managers</h1>
+      <img
+        src='https://api.sofascore.app/api/v1/unique-tournament/17/image'
+        alt=''
+        className='logo'
+      />
+    </header>
+  )
+}
+
+export default Header
