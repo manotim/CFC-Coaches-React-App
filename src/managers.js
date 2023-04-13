fff2065586970aaf76d99b705fbaa5d8ce159398
manoti <manotimike@gmail.com>
2023-04-13 18:04:57 +0300

    added managers database

---
+++ src/managers.js
@@ -0,0 +1,133 @@
+import imgFrank from './images/frank.jpg'
+import imgAncelloti from './images/ancelloti.png'
+import imgBenitez from './images/benitez.jpg'
+import imgConte from './images/conte.jpeg'
+import imgDimatteo from './images/di-matteo.jpg'
+import imgHiddink from './images/hiddink.jpg'
+import imgLampard from './images/lampard.png'
+import imgMourinho from './images/mourinho.png'
+import imgPotter from './images/potter.jpg'
+import imgSarri from './images/sarri.jpg'
+import imgScolari from './images/scolari.jpg'
+import imgTuchel from './images/tuchel.jpeg'
+import imgVillasBoas from './images/villas-Boas.jpeg'
+export const managers = [
+  {
+    id: 1,
+    fname: 'Frank',
+    lname: 'Lampard',
+    periodInCharge: 'PERIOD: 06/04/2023 - 30/06/2023',
+    flag: '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+    country: 'Country: England',
+    image: imgFrank,
+  },
+
+  {
+    id: 2,
+    fname: 'Graham',
+    lname: 'Potter',
+    periodInCharge: 'PERIOD: 08/09/2022 - 02/04/2023',
+    flag: '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+    country: 'Country: England',
+    image: imgPotter,
+  },
+  {
+    id: 3,
+    fname: 'Thomas',
+    lname: 'Tuchel',
+    periodInCharge: 'PERIOD: 26/01/2021 - 06/09/2022',
+    flag: '🇩🇪',
+    country: 'Country: Germany',
+    image: imgTuchel,
+  },
+  {
+    id: 4,
+    fname: 'Frank',
+    lname: 'Lampard',
+    periodInCharge: 'PERIOD: 04/07/2019 - 25/01/2021',
+    flag: '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+    country: 'Country: England',
+    image: imgLampard,
+  },
+  {
+    id: 5,
+    fname: 'Maurizio',
+    lname: 'Sarri',
+    periodInCharge: 'PERIOD: 14/07/2018 - 30/06/2019',
+    flag: '🇮🇹',
+    country: 'Country: Italy',
+    image: imgSarri,
+  },
+  {
+    id: 6,
+    fname: 'Antonio',
+    lname: 'Conte',
+    periodInCharge: 'PERIOD: 01/07/2016 - 13/07/2018',
+    flag: '🇮🇹',
+    country: 'Country: Italy',
+    image: imgConte,
+  },
+  {
+    id: 7,
+    fname: 'Guus',
+    lname: 'Hiddink',
+    periodInCharge: 'PERIOD: 20/12/2015 - 30/06/2016',
+    flag: '🇳🇱',
+    country: 'Country: Netherlands',
+    image: imgHiddink,
+  },
+  {
+    id: 8,
+    fname: 'José',
+    lname: 'Mourinho',
+    periodInCharge: 'PERIOD: 01/07/2013 - 17/12/2015',
+    flag: '🇵🇹',
+    country: 'Country: Portugal',
+    image: imgMourinho,
+  },
+  {
+    id: 9,
+    fname: 'Rafael',
+    lname: 'Benítez',
+    periodInCharge: 'PERIOD: 21/11/2012 - 30/06/2013',
+    flag: '🇪🇸',
+    country: 'Country: Spain',
+    image: imgBenitez,
+  },
+  {
+    id: 10,
+    fname: 'Roberto',
+    lname: 'Di Matteo',
+    periodInCharge: 'PERIOD: 05/03/2012 - 21/11/2012',
+    flag: '🇮🇹',
+    country: 'Country: Italy',
+    image: imgDimatteo,
+  },
+  {
+    id: 11,
+    fname: 'André',
+    lname: 'Villas-Boas',
+    periodInCharge: 'PERIOD: 01/07/2011 - 04/03/2012',
+    flag: '🇵🇹',
+    country: 'Country: Portugal',
+    image: imgVillasBoas,
+  },
+  {
+    id: 12,
+    fname: 'Carlo',
+    lname: 'Ancelloti',
+    periodInCharge: 'PERIOD: 01/07/2009 - 30/06/2011',
+    flag: '🇮🇹',
+    country: 'Country: Italy',
+    image: imgAncelloti,
+  },
+  {
+    id: 13,
+    fname: 'Luiz',
+    lname: 'Felipe Scolari',
+    periodInCharge: 'PERIOD: 01/07/2008 - 09/02/2009',
+    flag: '🇧🇷',
+    country: 'Country: Brazil',
+    image: imgScolari,
+  },
+]
